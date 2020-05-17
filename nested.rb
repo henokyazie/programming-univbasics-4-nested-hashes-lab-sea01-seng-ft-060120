1bef6bbeeaa22b4691258f75079fcd8521ac3781
brave-ping-4201 <h_yazie@yahoo.com>
2020-05-17 02:32:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -136,10 +136,10 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchie][languages: "Assembly"]
-    programmer_hash
+    
      programmer_hash[:dennis_ritchie] = {
   languages: ["Assembly"]
   }
+  programmer_hash
 
 end
