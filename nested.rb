4462e0a86c156b2f9023a4e3b5628b833f9e81e1
brave-ping-4201 <h_yazie@yahoo.com>
2020-05-17 02:31:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -138,5 +138,9 @@ def adding_to_dennis
     }
     programmer_hash[:dennis_ritchie][languages: "Assembly"]
     programmer_hash
+     programmer_hash[:yukihiro_matsumoto] = {
+  known_for: "Ruby",
+  languages: ["LISP", "C"]
+  }
 
 end
